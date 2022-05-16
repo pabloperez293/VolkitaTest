4f6c885b896c8745bbcae04cc1fafae298dd5667
pabloperez293 <56325243+pabloperez293@users.noreply.github.com>
2022-05-16 14:36:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
 
 
 
-// seller_id es el objeto
+// punto 1 - seller_id es el objeto 
 let seller_id = {
     item: 179571326,
     site_id : "MLA",    
@@ -24,3 +24,16 @@ for(let item in seller_id ) {
     console.log(item + " = " + seller_id[item]);
 
 }
+
+// punto 2 - Archivo Log
+
+// Se genera un objeto 
+const archivo ={
+    id: "id",
+    title: "titulo",
+    category_id: "categoria",
+    name: "nombre"
+}
+
+console.log([archivo]);
+

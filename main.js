3cf89318d25f48aa8e68c70b2eea81ce4ea64f3a
pabloperez293 <56325243+pabloperez293@users.noreply.github.com>
2022-05-16 14:42:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// let count = setInterval(updated, 40);
-// let up = 0 ;
-// let width = 0;
-// function updated () {
-//     const count = document.getElementById("vista");
-//     const hr = document.getElementById("hr");
-//     ++up;
-//     count.innerHTML = " Loading " + up + " %..";
-//     hr.style.width = up + "%";
-//     if (up = 100) {
-//         clearInterval(count);
-//         count.innerHTML = "done!";
-//     }
-// }
-
 
 
 // punto 1 - seller_id es el objeto 
